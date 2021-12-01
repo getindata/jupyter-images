f7ce89abc98f85ffa639fe3416cb4b1b3666c4a8
Mariusz Strzelecki <szczeles@gmail.com>
2021-12-01 11:01:52 +0100

    Adjusted spark version

--- jupyterlab-mlops/spark-driver-entrypoint.sh
+++ jupyterlab-mlops/spark-driver-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/local/spark-3.1.2-bin-hadoop3.2/kubernetes/dockerfiles/spark/entrypoint.sh driver "$@"
+exec /usr/local/spark-3.2.0-bin-hadoop3.2/kubernetes/dockerfiles/spark/entrypoint.sh driver "$@"
