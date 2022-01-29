03f9e8d937cd053694921b6566aa790a2ed74927
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2022-01-29 17:19:46 +0100

    Fixing mlflow startup script

--- jupyterlab-mlops/mlflow/start-mlflow.sh
+++ jupyterlab-mlops/mlflow/start-mlflow.sh
@@ -3,7 +3,9 @@
 MLFLOW_HOME=/home/$NB_USER/mlflow
 
 mkdir -p ${MLFLOW_HOME}
-mlflow db upgrade sqlite:///${MLFLOW_HOME}/mlflow.db
+if [ -f ${MLFLOW_HOME}/mlflow.db ]; then
+  mlflow db upgrade sqlite:///${MLFLOW_HOME}/mlflow.db
+fi
 mlflow server --host 0.0.0.0 \
     --default-artifact-root ${MLFLOW_HOME}/artifacts \
     --backend-store-uri sqlite:///${MLFLOW_HOME}/mlflow.db \
