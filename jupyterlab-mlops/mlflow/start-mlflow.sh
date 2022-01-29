5a2a18efa41aaefdc713fa90852e60bba888a387
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2022-01-29 17:06:35 +0100

    Minor Vertex home dir fixes

--- jupyterlab-mlops/mlflow/start-mlflow.sh
+++ jupyterlab-mlops/mlflow/start-mlflow.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash -i
 
-MLFLOW_HOME=/home/jovyan/mlflow
+MLFLOW_HOME=/home/$NB_USER/mlflow
 
 mkdir -p ${MLFLOW_HOME}
+mlflow db upgrade sqlite:///${MLFLOW_HOME}/mlflow.db
 mlflow server --host 0.0.0.0 \
     --default-artifact-root ${MLFLOW_HOME}/artifacts \
     --backend-store-uri sqlite:///${MLFLOW_HOME}/mlflow.db \
