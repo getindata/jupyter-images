7670f9b3090dec1c06acf654b8e20152b1458360
Andrzej Swatowski <andrzejswatowski@outlook.com>
2022-04-19 12:59:17 +0200

    Add DBT Docs tile

--- jupyterlab-dataops/resources/config/jupyter_notebook_config.py
+++ jupyterlab-dataops/resources/config/jupyter_notebook_config.py
@@ -31,3 +31,14 @@ c.ServerProxy.servers['cloudbeaver'] = {
         'icon_path': '/opt/tools/logos/cloudbeaver.svg',
     }
 }
+c.ServerProxy.servers['dbt_docs'] = {
+    'command': ['echo'],
+    'port': 9328,
+    'absolute_url': False,
+    'timeout': 180,
+    'new_browser_tab': False,
+    'launcher_entry': {
+        'title': "DBT Docs",
+        'icon_path': '/opt/tools/logos/docs.svg',
+    }
+}
