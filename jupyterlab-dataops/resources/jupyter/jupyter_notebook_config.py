cb6b2b01c0786cdd711950968d0b7d46dd1754eb
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-08-05 10:02:52 +0200

    chore: refactor variables and logic

--- jupyterlab-dataops/resources/jupyter/jupyter_notebook_config.py
+++ jupyterlab-dataops/resources/jupyter/jupyter_notebook_config.py
@@ -1,26 +1,23 @@
 import os
 import shutil
 c = get_config()  # noqa: F821
+HOME_PATH = "/home/jupyter"
 c.NotebookApp.open_browser = False
 c.ServerApp.token = ''
 c.ServerApp.password = ''
 c.ServerApp.base_url=os.getenv('NB_PREFIX', '')
-c.ServerApp.root_dir = f"/home/jupyter"
-c.ServerApp.notebook_dir = "/home/jupyter"
+c.ServerApp.root_dir = HOME_PATH
+c.ServerApp.notebook_dir = HOME_PATH
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
-c.FileCheckpoints.checkpoint_dir = '/home/jupyter/checkpoints'
-
+c.FileCheckpoints.checkpoint_dir = '{HOME_PATH}/checkpoints'
 
 def _codeserver_command():
     full_path = shutil.which('code-server')
     if not full_path:
         raise FileNotFoundError('Can not find code-server in $PATH')
-    working_dir = os.getenv("CODE_WORKINGDIR", None)
-    if working_dir is None:
-        working_dir = os.getenv("JUPYTER_SERVER_ROOT", ".")
-
+    working_dir = os.getenv("CODE_WORKINGDIR", None) or os.getenv("JUPYTER_SERVER_ROOT", ".")
     return [full_path, f'--port=7000', "--auth", "none", working_dir ]
 
 c.ServerProxy.servers = {}
