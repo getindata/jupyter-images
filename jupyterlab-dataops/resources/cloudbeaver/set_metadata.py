8edb880a9f70eb6065cea9eb3a3e9607dafdbd72
Jakub Dardzinski <kuba0221@gmail.com>
2022-10-05 00:11:56 +0200

    Add autoconfiguration of bigquery connection in CloudBeaver.
    Signed-off-by: Jakub Dardzinski <kuba0221@gmail.com>

--- jupyterlab-dataops/resources/cloudbeaver/set_metadata.py
+++ jupyterlab-dataops/resources/cloudbeaver/set_metadata.py
@@ -1,6 +1,10 @@
 from google.cloud import compute_v1
 import requests
+import jinja2
+import os
 
+TEMPLATE_PATH = os.getenv("TEMPLATE_PATH", "/opt/cloudbeaver/conf/initial-data-sources.conf.j2")
+SOURCES_PATH = os.getenv("SOURCES_PATH", "/opt/cloudbeaver/conf/initial-data-sources.conf")
 
 def get_gooogle_instance_property(attribute_name):
     try:
@@ -25,7 +29,20 @@ def get_google_project_attribute(attribute_name):
     except:
         return None
 
+def render_template(project_id):
+    loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(TEMPLATE_PATH))
+    env = jinja2.Environment(loader=loader)
+    try:
+        template = env.get_template(os.path.basename(TEMPLATE_PATH))
+    except jinja2.exceptions.TemplateNotFound:
+        return None
+    return template.render({'project_id': project_id})
 
+def configure_bigquery_connection(project_id):
+    rendered = render_template(project_id)
+    with open(SOURCES_PATH, 'w') as f:
+        f.write(rendered)
+    
 try:
     instance = get_gooogle_instance_property('hostname').split('.')[0]
     zone = get_gooogle_instance_property('zone').split('/')[-1]
@@ -40,5 +57,7 @@ try:
 
     instance_client.set_metadata(project=project, zone=zone, instance=instance, metadata_resource={
                                  "items": [{"key": k, "value": v} for k, v in entries.items()], "fingerprint": existing_meta.fingerprint})
+    
+    configure_bigquery_connection(project)
 except:
     pass
