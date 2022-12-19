bc8f779583c4a86b5c26b979b4a9a1ff51069d93
Mariusz Strzelecki <szczeles@gmail.com>
2022-12-19 12:34:11 +0100

    Kedro-viz tile

--- jupyterlab-mlops/jupyter_notebook_config.py
+++ jupyterlab-mlops/jupyter_notebook_config.py
@@ -2,35 +2,45 @@ import os
 
 c = get_config()  # noqa: F821
 c.NotebookApp.open_browser = False
-c.ServerApp.token = ''
-c.ServerApp.password = ''
-c.ServerApp.base_url=os.getenv('NB_PREFIX', '')
+c.ServerApp.token = ""
+c.ServerApp.password = ""
+c.ServerApp.base_url = os.getenv("NB_PREFIX", "")
 c.ServerApp.root_dir = f"/home/{os.environ['NB_USER']}"
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
 
 c.ServerProxy.servers = {
-    'mlflow': {
-            'command': ['/usr/local/bin/start-mlflow.sh'],
-            'port': 5000,
-            'absolute_url': False,
-            'timeout': 30,
-            'launcher_entry': {
-                'title': "mlflow",
-                'icon_path': '/usr/local/share/mlflow-logo.svg'
-        }
-    }
+    "mlflow": {
+        "command": ["/usr/local/bin/start-mlflow.sh"],
+        "port": 5000,
+        "absolute_url": False,
+        "timeout": 30,
+        "launcher_entry": {
+            "title": "mlflow",
+            "icon_path": "/usr/local/share/mlflow-logo.svg",
+        },
+    },
+    "kedro-viz": {
+        "command": ["echo"],  # any command is OK, as process is started externally
+        "port": 4141,
+        "absolute_url": False,
+        "timeout": 30,
+        "launcher_entry": {
+            "title": "Kedro-Viz",
+            "icon_path": "/usr/local/share/kedro-logo.svg",
+        },
+    },
 }
 
-if os.getenv('VSCODE_VERSION', 'NONE') != 'NONE':
-    c.ServerProxy.servers['vscode'] = {
-        'command': ['/bin/bash', '-c', '/usr/local/bin/start-vscode.sh', '{port}'],
-        'port': 7000,
-        'absolute_url': False,
-        'timeout': 30,
-        'launcher_entry': {
-            'title': "VSCode",
-            'icon_path': '/usr/local/share/vscode-logo.svg',
-        }
+if os.getenv("VSCODE_VERSION", "NONE") != "NONE":
+    c.ServerProxy.servers["vscode"] = {
+        "command": ["/bin/bash", "-c", "/usr/local/bin/start-vscode.sh", "{port}"],
+        "port": 7000,
+        "absolute_url": False,
+        "timeout": 30,
+        "launcher_entry": {
+            "title": "VSCode",
+            "icon_path": "/usr/local/share/vscode-logo.svg",
+        },
     }
