7a6078f3e8f7ec6945ea0d868e78054aa468e49b
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-12-23 12:09:54 +0100

    chore: update settings and add permision for folder

--- jupyterlab-dataops/resources/snowsql/entrypoint.sh
+++ jupyterlab-dataops/resources/snowsql/entrypoint.sh
@@ -5,4 +5,5 @@ mkdir -p /home/jovyan/.vscode
 cp /var/tmp/config /home/jovyan/.snowsql/config
 cp /var/tmp/settings.json /home/jovyan/.vscode/settings.json
 chown jovyan:users -R /home/jovyan/.snowsql
+chown jovyan:users -R /home/jovyan/.vscode
 start-notebook.sh
