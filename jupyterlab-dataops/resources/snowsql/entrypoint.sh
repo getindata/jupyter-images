983c2c772779140a62d235a10c903a55a418bf9e
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-12-23 09:49:00 +0100

    chore: move settings to default folde

--- jupyterlab-dataops/resources/snowsql/entrypoint.sh
+++ jupyterlab-dataops/resources/snowsql/entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 mkdir -p /home/jovyan/.snowsql
 cp /var/tmp/config /home/jovyan/.snowsql/config
+cp /var/tmp/settings.json /home/jovyan/.vscode/settings.json
 chown jovyan:users -R /home/jovyan/.snowsql
 start-notebook.sh
