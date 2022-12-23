3c33cc4ce0ebc92170be88c38ef3c8bcc86d7050
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-12-23 10:26:12 +0100

    chore: create folder for default settings.json

--- jupyterlab-dataops/resources/snowsql/entrypoint.sh
+++ jupyterlab-dataops/resources/snowsql/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 mkdir -p /home/jovyan/.snowsql
+mkdir -p /home/jovyan/.vscode
 cp /var/tmp/config /home/jovyan/.snowsql/config
 cp /var/tmp/settings.json /home/jovyan/.vscode/settings.json
 chown jovyan:users -R /home/jovyan/.snowsql
