ef98db411e28bb93af4be22cda22471633e15d04
Piotr Sierkin <psierkin@gmail.com>
2022-12-28 09:54:12 +0100

    feat: add new addons and settings.json (#45)
    * feat: add new addons and settings.json
    
    * chore: move settings to default folde
    
    * chore: create folder for default settings.json
    
    * chore: update settings and add permision for folder
    
    * chore: bump code server
    
    * feat: add .bashrc and update settings.json
    
    Co-authored-by: Piotr Sierkin <piotr.sierkin@getindata.com>

--- jupyterlab-dataops/resources/snowsql/entrypoint.sh
+++ jupyterlab-dataops/resources/snowsql/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 mkdir -p /home/jovyan/.snowsql
+mkdir -p /home/jovyan/.vscode
 cp /var/tmp/config /home/jovyan/.snowsql/config
+cp /var/tmp/settings.json /home/jovyan/.vscode/settings.json
+cp /var/tmp/.bashrc /home/jovyan/.bashrc
 chown jovyan:users -R /home/jovyan/.snowsql
+chown jovyan:users -R /home/jovyan/.vscode
+chown jovyan:users -R /home/jovyan/.bashrc
 start-notebook.sh
