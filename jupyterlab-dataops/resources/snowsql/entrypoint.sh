c56b08c136c69aa3b3585eb372e988b26ee4efe5
Piotr Sierkin <psierkin@gmail.com>
2023-01-03 14:56:04 +0100

    chore: add permision for ssh (#46)
    * chore: add permision for ssh
    
    * chore: test with chmod
    
    * test
    
    * tst
    
    Co-authored-by: Piotr Sierkin <piotr.sierkin@getindata.com>

--- jupyterlab-dataops/resources/snowsql/entrypoint.sh
+++ jupyterlab-dataops/resources/snowsql/entrypoint.sh
@@ -2,10 +2,13 @@
 set -e
 mkdir -p /home/jovyan/.snowsql
 mkdir -p /home/jovyan/.vscode
+mkdir -p /home/jovyan/.ssh
 cp /var/tmp/config /home/jovyan/.snowsql/config
 cp /var/tmp/settings.json /home/jovyan/.vscode/settings.json
 cp /var/tmp/.bashrc /home/jovyan/.bashrc
 chown jovyan:users -R /home/jovyan/.snowsql
 chown jovyan:users -R /home/jovyan/.vscode
 chown jovyan:users -R /home/jovyan/.bashrc
+chown jovyan:users -R /home/jovyan/.ssh
+chmod -R 700 /home/jovyan/.ssh
 start-notebook.sh
